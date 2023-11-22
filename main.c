eedea3efde6cfb77d7ba7c43e9502a0869352a1f
guinb0 <128229850+guinb0@users.noreply.github.com>
2023-11-21 21:03:34 -0300

    Update main.c

--- main.c
+++ main.c
@@ -25,3 +25,40 @@ void empilhar(Pilha *pilha, int disco) {
         pilha->items[++pilha->top] = disco;
     }
 }
+void empilhar(Pilha *pilha, int disco) {
+    if (!estaCheia(pilha)) {
+        pilha->items[++pilha->top] = disco;
+    }
+}
+int desempilhar(Pilha *pilha) {
+    if (!estaVazia(pilha)) {
+        return pilha->items[pilha->top--];
+    }
+    return -1; // Indica que a pilha está vazia
+}
+
+void imprimirTorres(Pilha *torres, int numDiscos) {
+    printf("\nEstado Atual das Torres:\n\n");
+
+    for (int i = numDiscos - 1; i >= 0; i--) {
+        for (int j = 0; j < 3; j++) {
+            if (i <= torres[j].top) {
+                for (int k = 0; k < torres[j].items[i]; k++) {
+                    printf("* ");
+                }
+            } else {
+                printf("| ");
+            }
+            printf("   ");
+        }
+        printf("\n");
+    }
+
+    printf("\n");
+
+    for (int i = 0; i < 3; i++) {
+        printf("  Torre %d  ", i + 1);
+    }
+
+    printf("\n\n");
+}
